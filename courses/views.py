23b66f45885dcc4b484ba0d234bc84132dfc0d2e
Sebastian <sebastianschulz.dev@gmail.com>
2020-04-04 16:32:37 +0200

    Creating class-based views

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Course
+
+
+class ManagerCourseListView(ListView):
+    model = Course
+    template_name = 'course/manage/course/list.html'
+
+    def get_queryset(self):
+        qs = super(ManagerCourseListView, self).get_queryset()
+        return qs.filter(owner=self.request.user)
 
 # Create your views here.
