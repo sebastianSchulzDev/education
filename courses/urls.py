0b4847c3d4068653983b2b98c18514c9edb98a73
Sebastian <sebastianschulz.dev@gmail.com>
2020-05-03 10:19:27 +0200

    Displaying courses

--- courses/urls.py
+++ courses/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from courses.views import CourseListView
 
 urlpatterns = [path('mine/', views.ManagerCourseListView.as_view(), name='manage_course_list'),
                path('create/', views.CourseCreateView.as_view(), name='course_create'),
@@ -13,4 +14,7 @@ urlpatterns = [path('mine/', views.ManagerCourseListView.as_view(), name='manage
                path('content/<int:id>/delete', views.ContentDeleteView.as_view(), name='module_content_delete'),
                path('module/<int:module_id>/', views.ModuleContentListView.as_view(), name='module_content_list'),
                path('module/order/', views.ModuleOrderView.as_view(), name='module_order'),
-               path('content/order/', views.ContentOrderView.as_view(), name='content_order')]
+               path('content/order/', views.ContentOrderView.as_view(), name='content_order'),
+               path('subject/<slug:subject>)/', views.CourseListView.as_view(), name='course_list_subject'),
+               path('<slug:slug>/', views.CourseDetailView.as_view(), name='course_detail'),
+                ]
--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.list import ListView
-from .models import Course
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
@@ -8,8 +7,10 @@ from django.views.generic.base import TemplateResponseMixin, View
 from .forms import ModuleFormSet
 from django.forms.models import modelform_factory
 from django.apps import apps
-from .models import Module, Content
+from .models import Module, Content, Course, Subject
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+from django.db.models import Count
+from django.views.generic.detail import DetailView
 
 
 class ManagerCourseListView(ListView):
@@ -173,3 +174,21 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
         for id, order in self.request_json.items():
             Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects, 'subject': subject, 'courses': courses})
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
